61432bce4255c6288907ab85fc0d91015425ab89
Christian Stein <sormuras@gmail.com>
2022-03-06 10:14:41 +0100

    Release 0.1

--- project.java
+++ project.java
@@ -1,5 +1,5 @@
 record project() {
-  public static String VERSION = "main";
+  public static String VERSION = "0.1";
 
   public static void main(String... args) {
     System.out.println("Java Project Processor " + VERSION);
